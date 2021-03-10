c445c4a1465d090eeb3176a4937e46fc2c902fef
zocker-160 <zocker1600@posteo.net>
2021-03-11 00:33:30 +0100

    add initial version

---
+++ src/lcd.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+import time
+import RPi.GPIO as GPIO
+
+LCD_RS = 4
+LCD_E  = 17
+LCD_DATA4 = 18
+LCD_DATA5 = 22
+LCD_DATA6 = 23
+LCD_DATA7 = 24
+
+LCD_WIDTH = 16 		# number of chars for one line
+LCD_LINE_1 = 0x80 	# address first line
+LCD_LINE_2 = 0xC0 	# address second line
+LCD_CHR = GPIO.HIGH
+LCD_CMD = GPIO.LOW
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+def lcd_send_byte(bits, mode):
+	# Pins auf LOW setzen
+	GPIO.output(LCD_RS, mode)
+	GPIO.output(LCD_DATA4, GPIO.LOW)
+	GPIO.output(LCD_DATA5, GPIO.LOW)
+	GPIO.output(LCD_DATA6, GPIO.LOW)
+	GPIO.output(LCD_DATA7, GPIO.LOW)
+	if bits & 0x10 == 0x10:
+	  GPIO.output(LCD_DATA4, GPIO.HIGH)
+	if bits & 0x20 == 0x20:
+	  GPIO.output(LCD_DATA5, GPIO.HIGH)
+	if bits & 0x40 == 0x40:
+	  GPIO.output(LCD_DATA6, GPIO.HIGH)
+	if bits & 0x80 == 0x80:
+	  GPIO.output(LCD_DATA7, GPIO.HIGH)
+	time.sleep(E_DELAY)    
+	GPIO.output(LCD_E, GPIO.HIGH)  
+	time.sleep(E_PULSE)
+	GPIO.output(LCD_E, GPIO.LOW)  
+	time.sleep(E_DELAY)      
+	GPIO.output(LCD_DATA4, GPIO.LOW)
+	GPIO.output(LCD_DATA5, GPIO.LOW)
+	GPIO.output(LCD_DATA6, GPIO.LOW)
+	GPIO.output(LCD_DATA7, GPIO.LOW)
+	if bits&0x01==0x01:
+	  GPIO.output(LCD_DATA4, GPIO.HIGH)
+	if bits&0x02==0x02:
+	  GPIO.output(LCD_DATA5, GPIO.HIGH)
+	if bits&0x04==0x04:
+	  GPIO.output(LCD_DATA6, GPIO.HIGH)
+	if bits&0x08==0x08:
+	  GPIO.output(LCD_DATA7, GPIO.HIGH)
+	time.sleep(E_DELAY)    
+	GPIO.output(LCD_E, GPIO.HIGH)  
+	time.sleep(E_PULSE)
+	GPIO.output(LCD_E, GPIO.LOW)  
+	time.sleep(E_DELAY)  
+
+def display_init():
+	lcd_send_byte(0x33, LCD_CMD)
+	lcd_send_byte(0x32, LCD_CMD)
+	lcd_send_byte(0x28, LCD_CMD)
+	lcd_send_byte(0x0C, LCD_CMD)  
+	lcd_send_byte(0x06, LCD_CMD)
+	lcd_send_byte(0x01, LCD_CMD)  
+
+def lcd_message(message):
+	message = message.ljust(LCD_WIDTH," ")  
+	for i in range(LCD_WIDTH):
+	  lcd_send_byte(ord(message[i]),LCD_CHR)
+
+class LCD:
+	def __init__(self) -> None:
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setwarnings(False)
+		GPIO.setup(LCD_E, GPIO.OUT)
+		GPIO.setup(LCD_RS, GPIO.OUT)
+		GPIO.setup(LCD_DATA4, GPIO.OUT)
+		GPIO.setup(LCD_DATA5, GPIO.OUT)
+		GPIO.setup(LCD_DATA6, GPIO.OUT)
+		GPIO.setup(LCD_DATA7, GPIO.OUT)
+
+		display_init()
+	
+	def writeLine1(self, msg: str):
+		lcd_send_byte(LCD_LINE_1, LCD_CMD)
+		lcd_message(msg)
+		time.sleep(0.1)
+
+	def writeLine2(self, msg: str):
+		lcd_send_byte(LCD_LINE_2, LCD_CMD)
+		lcd_message(msg)
+		time.sleep(0.1)	
+	
+	def clean(self):
+		GPIO.cleanup()
---
+++ src/main.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python3
+
+from os import write
+import sys
+import signal
+import time
+
+from threading import Thread
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from twisted.internet import reactor
+
+from lcd import LCD
+
+API_KEY = "YOUR KEY"
+API_SECRET = "YOUR SECRET"
+
+currentPrice: float = 0
+
+def getMethod(client: Client, lcd: LCD):
+    while True:
+        try:
+            #client.ping()
+            #price: dict = client.get_avg_price(symbol="BTCEUR")
+            price: dict = client.get_symbol_ticker(symbol="BTCEUR")
+        except KeyboardInterrupt:
+            print("exiting....")
+            sys.exit()
+
+        lcd.writeLine2(price.get("price"))
+        time.sleep(1)
+        #print(price.get("price"))
+
+def wsMethod(client: Client):
+    bm = BinanceSocketManager(client)
+
+    signal.signal(signal.SIGINT, _closeConnection)
+    signal.signal(signal.SIGTERM, _closeConnection)
+
+    bm.start_trade_socket("BTCEUR", _wsCallback)
+    bm.start()
+
+    global writeThread
+    global running
+    running = True
+    writeThread = Thread(target=writeLCD)
+    writeThread.start()
+
+def _wsCallback(msg: dict):
+    global currentPrice
+    currentPrice = float(msg.get("p"))
+    print(f'Preis: {msg.get("p")}')
+
+def _closeConnection(signum, frame):
+    print("exiting...")
+    global running
+    running = False
+    reactor.stop()
+    writeThread.join()
+
+def writeLCD():
+    global currentPrice
+    global running
+
+    while running:
+        print(f"LCD {currentPrice}")
+        #lcd.writeLine2(str(currentPrice))
+        time.sleep(1)
+
+if __name__ == "__main__":
+    lcd = LCD()
+    client = Client(api_key=API_KEY, api_secret=API_SECRET)
+    #all = client.get_all_tickers()
+
+    # init screen
+    lcd.clean()
+    lcd.writeLine1("BTC / EUR")
+
+    getMethod(client, lcd)
+    #wsMethod(client)
