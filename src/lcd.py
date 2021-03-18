3bbd09a88f76e584468f393aff37a71c165c667c
zocker-160 <zocker1600@posteo.net>
2021-03-18 17:41:57 +0100

    update write function

--- src/lcd.py
+++ src/lcd.py
@@ -3,16 +3,23 @@ import RPi.GPIO as GPIO
 
 class LCD:
 
+    line1 = ""
+    line2 = ""
+
     def __init__(self):
         self.lcd = CharLCD(pin_rs=26, pin_e=19, pins_data=[13, 6, 5, 11], numbering_mode=GPIO.BCM)
 
     def writeLine1(self, text: str):
-        self.clear()
-        self.lcd.write_string(f"{text}")
+        self.line1 = text
+        self._toDisplay()
 
     def writeLine2(self, text: str):
+        self.line2 = text
+        self._toDisplay()
+
+    def _toDisplay(self):
         self.clear()
-        self.lcd.write_string(f"\n{text}")
+        self.lcd.write_string(f"{self.line1}\n{self.line2}")
 
     def clear(self):
         self.lcd.clear()
